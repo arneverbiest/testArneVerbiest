2a44810582bb5f067cf2aff16bce9f2138454a17
arneverbiest <Arne.verbiest@gmail.com>
2023-01-09 14:53:05 +0100

    Add project files.

---
+++ allphi/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+
+/*
+ * 
+ */
+public class Solution
+{
+    public static char lostCharNW(string stringSent, string stringRec)
+    {
+        char answer = '*';
+        if (stringSent != stringRec)
+        {
+            for (int i = 0; i < stringSent.Length; i++)
+            {
+                if (stringSent.Length < stringRec.Length)
+                {
+                    Console.WriteLine(stringSent.LastIndexOf(stringSent));
+                }
+
+                /*
+                                if (stringSent[i] != stringRec[i])
+                                {
+                                    Console.WriteLine(stringRec[i]);
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("the output is the same");
+                        }
+                 */
+
+
+            }
+        }
+                return answer;
+        
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] input;
+        //input for stringSent
+        string stringSent = (Console.ReadLine());
+
+        //input for stringRec
+        string stringRec = (Console.ReadLine());
+
+
+        char result = lostCharNW(stringSent, stringRec);
+        Console.Write(result);
+
+    }
+}
---
+++ allphi3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Xml;
+
+
+/*
+ * 
+ */
+public class Solution
+{
+    public static int busRemaining(int[,] busStation)
+    {
+
+        int answer = 0;
+
+
+
+
+
+
+        for (int i = 1; i < busStation.GetLength(0); i++)
+        {
+            if (busStation[i, 1] >= busStation[i - 1, 1])
+            {
+                Console.WriteLine($"{busStation[i, 1]} >= {busStation[i - 1, 1]}");
+                if (busStation[i, 0] <= busStation[i - 1, 1])
+                {
+                    Console.WriteLine($"{busStation[i, 0]} <= {busStation[i - 1, 1]}");
+                    answer++;
+
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void Main(string[] args)
+    {
+        string[] input;
+        //input for busStation
+        input = Console.ReadLine().Split(' ');
+        int busStation_row = int.Parse(input[0]);
+        int busStation_col = int.Parse(input[1]);
+        int[,] busStation = new int[busStation_row, busStation_col];
+        for (int idx = 0; idx < busStation_row; idx++)
+        {
+            input = Console.ReadLine().Split(' ');
+            for (int jdx = 0; jdx < busStation_col; jdx++)
+            {
+                busStation[idx, jdx] = int.Parse(input[jdx]);
+            }
+        }
+
+
+
+
+        int result = busRemaining(busStation);
+        Console.WriteLine($"there will be {result} bus routes less");
+        Console.Write(result);
+
+    }
+}
